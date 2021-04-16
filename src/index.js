dad1d2aabe719a05bb6be9889b3d1ae5b25e886b
Thamires Lopes <thamiresl686@gmail.com>
2021-04-16 11:57:08 -0300

    Set required backend configs in index js

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const cors = require('cors');
+const express = require("express");
+const morgan = require('morgan');
+const mongoose = require("mongoose");
+
+const Routes = require("./routes/routes");
+require('dotenv').config();
+
+const { HTTP_PORT, MONGO_URL } = process.env;
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to mongoDB')
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use("/api", Routes);
+
+app.get("/", (request, response) => {
+    response.json({ message: "Hello World" });
+});
+
+app.listen(HTTP_PORT, () => {
+    console.log(`Rodando na porta ${HTTP_PORT}`);
+});
