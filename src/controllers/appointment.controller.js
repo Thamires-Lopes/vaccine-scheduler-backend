5e4d9da12f7a594dcbe07f49f8ae8a18d64a5f6a
Thamires Lopes <thamiresl686@gmail.com>
2021-04-17 16:42:09 -0300

    Basic age and availability checks

--- src/controllers/appointment.controller.js
+++ src/controllers/appointment.controller.js
@@ -3,6 +3,8 @@ const AppointmentModel = require('../models/appointment.model');
 
 const appointmentValidateForm = validateForm(appointmentSchema);
 
+const calculateAge = require('../utils/checks')
+
 class AppointmentController {
 
     async index(req, res) {
@@ -14,11 +16,57 @@ class AppointmentController {
         try {
             appointmentValidateForm(req.body);
 
+            const { vaccineDay, vaccineTime, birthday } = req.body;
             //todo before storing need to check if that the date and time is available
+            //need refactoring
+            const appointments = await AppointmentModel.find({ vaccineDay })
 
-            const newAppointment = await AppointmentModel.create(req.body);
+            console.log(appointments.length)
+            if (appointments.length < 6) {
+                const time = new Date(vaccineTime).getHours()
+                const sameTime = appointments.filter((info) => (new Date(info.vaccineTime).getHours()) === time)
 
-            res.send({ appointment: newAppointment })
+                if (sameTime.length < 2) {
+                    const newAppointment = await AppointmentModel.create(req.body);
+
+                    res.send({ appointment: newAppointment })
+                } else {
+                    if (calculateAge(birthday) >= 65) {
+                        const young = sameTime.filter((info) => calculateAge(info.birthday) < 65)
+                        if (young.length > 0) {
+                            if (young.length > 1) {
+                                young.sort((a, b) => {
+                                    if (calculateAge(a.birthday) > calculateAge(b.birthday)) {
+                                        return 1
+                                    }
+                                    if (a.name < b.name) {
+                                        return -1;
+                                    }
+                                    return 0;
+                                })
+                            }
+                            const { id } = young[0]
+                            const deleted = await AppointmentModel.findByIdAndDelete(id);
+                            if (deleted) {
+                                const newAppointment = await AppointmentModel.create(req.body);
+                                res.send({ appointment: newAppointment })
+                            }
+
+                        } else {
+                            console.log("Horário cheio")
+                            throw new Error("Horário cheio")
+                        }
+                        console.log("idoso")
+                    } else {
+                        console.log("Horário cheio")
+                        throw new Error("Horário cheio")
+                    }
+                }
+
+            } else {
+                throw new Error("Dia cheio")
+            }
+
 
         } catch (error) {
             res.status(400).send({ message: "Invalidate fields", error: error.message })
